a0c03eda1e64778e53cb10ff419bb6e5372f2e8c
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-05 17:12:27 +0200

    updated imports

--- Package.swift
+++ Package.swift
@@ -27,6 +27,18 @@ let package = Package(
             url: "https://github.com/leviouwendijk/Interfaces.git",
             branch: "master"
         ),
+        .package(
+            url: "https://github.com/leviouwendijk/Economics.git",
+            branch: "master"
+        ),
+        .package(
+            url: "https://github.com/leviouwendijk/Implementations.git",
+            branch: "master"
+        ),
+        .package(
+            url: "https://github.com/leviouwendijk/Structures.git",
+            branch: "master"
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -37,6 +49,9 @@ let package = Package(
                 .product(name: "plate", package: "plate"),
                 .product(name: "ViewComponents", package: "ViewComponents"),
                 .product(name: "Interfaces", package: "Interfaces"),
+                .product(name: "Economics", package: "Economics"),
+                .product(name: "Implementations", package: "Implementations"),
+                .product(name: "Structures", package: "Structures"),
             ],
             resources: [
                 .process("Resources")
@@ -49,6 +64,9 @@ let package = Package(
                 .product(name: "plate", package: "plate"),
                 .product(name: "ViewComponents", package: "ViewComponents"),
                 .product(name: "Interfaces", package: "Interfaces"),
+                .product(name: "Economics", package: "Economics"),
+                .product(name: "Implementations", package: "Implementations"),
+                .product(name: "Structures", package: "Structures"),
             ]
         ),
     ]
---
+++ Sources/Compositions/mailer/views/mailer-stdout-log.swift
@@ -0,0 +1,64 @@
+import Foundation
+import SwiftUI
+import Combine
+import plate
+import ViewComponents
+import Economics
+import Interfaces
+import Implementations
+
+public class MailerViewModel: ObservableObject {
+    @Published public var mailerOutput: String = ""
+    @Published public var sharedMailerCommandCopy: String = ""
+}
+
+public struct MailerStandardOutput: View {
+    @EnvironmentObject public var vm: MailerViewModel
+
+    public var body: some View {
+        VStack {
+            VStack {
+                SectionTitle(title: "mailer command", width: 150)
+
+                Button(action: {
+                    NSPasteboard.general.clearContents()
+                    NSPasteboard.general.setString(vm.sharedMailerCommandCopy, forType: .string)
+                }) {
+                    Text(vm.sharedMailerCommandCopy)
+                    .bold()
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(5)
+                }
+                .buttonStyle(PlainButtonStyle())
+                .frame(maxWidth: 800, alignment: .center)
+            } 
+
+            VStack {
+                SectionTitle(title: "stdout log", width: 150)
+
+                ScrollView {
+                    Text(vm.mailerOutput)
+                    .font(.system(.body, design: .monospaced))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(4)
+                }
+                .background(Color.black.opacity(0.05))
+                .cornerRadius(6)
+
+                .frame(maxWidth: 800, alignment: .center)
+
+                HStack {
+                    StandardButton(
+                        type: .copy, 
+                        title: "Copy stdout"
+                    ) {
+                        copyToClipboard(vm.mailerOutput)
+                    }
+                }
+                // .frame(maxWidth: .infinity, alignment: .center)
+                .padding(.top, 10)
+            }
+        }
+    }
+}
