7232bcb00c96b83bf34651525df2d12c1a52ba90
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-06 16:36:31 +0200

    quota tier view changes

--- Sources/Compositions/Responder/views/quota/quota-view.swift
+++ Sources/Compositions/Responder/views/quota/quota-view.swift
@@ -16,43 +16,49 @@ public struct QuotaView: View {
 
     public var body: some View {
         VStack {
-            QuotaInputsView(viewmodel: quotaVm)
+            HStack {
+                QuotaInputsView(viewmodel: quotaVm)
 
-            if quotaVm.isLoading {
-                ProgressView("Computing quota…")
-                    .padding(.top, 16)
-            }
+                VStack {
+                    if quotaVm.isLoading {
+                        ProgressView("Computing quota…")
+                            .padding(.top, 16)
+                    }
 
-            if !(quotaVm.errorMessage.isEmpty) {
-                if quotaVm.hasEmptyInputs {
-                    NotificationBanner(
-                        type: .info,
-                        message: "Enter inputs"
-                    )
-                } else {
-                    NotificationBanner(
-                        type: .warning,
-                        message: quotaVm.errorMessage
-                    )
+                    if !(quotaVm.errorMessage.isEmpty) {
+                        if quotaVm.hasEmptyInputs {
+                            NotificationBanner(
+                                type: .info,
+                                message: "Enter inputs"
+                            )
+                        } else {
+                            NotificationBanner(
+                                type: .warning,
+                                message: quotaVm.errorMessage
+                            )
+                        }
+                    }
+                    else if let quota = quotaVm.loadedQuota {
+                        QuotaTierListView(quota: quota)
+                            // .padding(.top, 12)
+                            // .padding(.bottom, 16)
+
+                    }
+                    else {
+                        NotificationBanner(
+                            type: .info,
+                            message: "Enter quote values above"
+                        )
+                        .padding(.top, 16)
+                    }
                 }
+                .frame(maxHeight: .infinity)
+                .frame(maxWidth: .infinity)
             }
 
-            else if let quota = quotaVm.loadedQuota {
-                QuotaTierListView(quota: quota)
-                    // .padding(.top, 12)
-                    // .padding(.bottom, 16)
-
+            if let quota = quotaVm.loadedQuota {
                 QuotaTierActionsView(quota: quota, clientIdentifier: viewmodel.clientIdentifier)
             }
-            else {
-                NotificationBanner(
-                    type: .info,
-                    message: "Enter quote values above"
-                )
-                .padding(.top, 16)
-            }
         }
-        .frame(maxHeight: .infinity)
-        .frame(maxWidth: 400)
     }
 }
