df76d29e1634801e82e94faf439b2dcfc6904ba8
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-06 16:08:59 +0200

    typo

--- Sources/Compositions/Responder/views/quota/quota-view.swift
+++ Sources/Compositions/Responder/views/quota/quota-view.swift
@@ -5,7 +5,7 @@ import ViewComponents
 import Implementations
 
 public struct QuotaView: View {
-    @ObsersvedObject public var viewmodel: ResponderViewModel
+    @ObservedObject public var viewmodel: ResponderViewModel
     
     public init(
         viewmodel: ResponderViewModel
