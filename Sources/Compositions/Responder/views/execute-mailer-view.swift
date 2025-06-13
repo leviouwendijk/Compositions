7e2d5903cc89ab04836e3d4730da4b00a0819ab4
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-13 22:58:33 +0200

    removing restriction disabled execute mailer view

--- Sources/Compositions/Responder/views/execute-mailer-view.swift
+++ Sources/Compositions/Responder/views/execute-mailer-view.swift
@@ -65,7 +65,7 @@ public struct ExecuteMailerView: View {
                     }
                     .disabled(viewmodel.isSendingEmail)
                     .disabled(viewmodel.apiPathVm.routeOrEndpointIsNil)
-                    .disabled(viewmodel.noContactSelectedButIsRequired)
+                    // .disabled(viewmodel.noContactSelectedButIsRequired)
                 }
                 .padding(.top, 10)
             }
