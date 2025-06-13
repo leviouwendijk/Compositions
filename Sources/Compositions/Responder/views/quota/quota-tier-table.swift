21082abb8ffd720afbd46e68688939a3e64813be
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-13 22:19:24 +0200

    minor incr

--- Sources/Compositions/Responder/views/quota/quota-tier-table.swift
+++ Sources/Compositions/Responder/views/quota/quota-tier-table.swift
@@ -202,7 +202,7 @@ public struct LabelColumnView: View {
                 Text("")
                     .font(.subheadline).bold()
                     .frame(maxWidth: .infinity)
-                    .padding(.vertical, 36)
+                    .padding(.vertical, 38)
 
                 Divider()
 
