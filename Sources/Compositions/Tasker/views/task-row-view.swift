67580ef25d3c6918f4367d1f08fb94c1186e6eca
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-06 18:42:09 +0200

    clear

--- Sources/Compositions/Tasker/views/task-row-view.swift
+++ Sources/Compositions/Tasker/views/task-row-view.swift
@@ -141,7 +141,8 @@ public struct TaskRowView: View {
         .padding(8)
         .background(
             RoundedRectangle(cornerRadius: 6)
-                .fill(Color.gray.opacity(0.1))
+                .fill(Color.clear)
+                // .fill(Color.gray.opacity(0.1))
                 // .fill(Color(NSColor.controlBackgroundColor))
                 .overlay(
                     RoundedRectangle(cornerRadius: 6)
