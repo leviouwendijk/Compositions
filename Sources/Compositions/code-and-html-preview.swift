0ca5bb0cbe8b5e558ef1bc05792b4cb2122bdf9a
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-21 08:39:33 +0200

    vim

--- Sources/Compositions/code-and-html-preview.swift
+++ Sources/Compositions/code-and-html-preview.swift
@@ -17,3 +17,19 @@ public struct CodeAndPreviewView: View {
         .frame(minHeight: 400)
     }
 }
+
+public struct VimCodeAndPreviewView: View {
+    @StateObject public var viewmodel = HTMLPreviewViewModel()
+
+    public init() {}
+
+    public var body: some View {
+        HSplitView {
+            VimCodeEditor(text: $viewmodel.html)
+                .frame(minWidth: 300)
+            HTMLPreview(html: $viewmodel.html)
+                .frame(minWidth: 300)
+        }
+        .frame(minHeight: 400)
+    }
+}
