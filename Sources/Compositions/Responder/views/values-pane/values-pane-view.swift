23f1afd4006eab0c59917a1193c720cf1ce91c58
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-09-25 16:40:57 +0200

    fixing equatable viewlogic for endpoints

--- Sources/Compositions/Responder/views/values-pane/values-pane-view.swift
+++ Sources/Compositions/Responder/views/values-pane/values-pane-view.swift
@@ -7,36 +7,47 @@ import Contacts
 import Economics
 import Implementations
 
+@MainActor
+fileprivate func isAppt(_ viewmodel: ResponderViewModel) -> Bool {
+    return viewmodel.apiPathVm.selectedRoute == .appointment
+}
+
+@MainActor
+fileprivate func isAvailReq(_ viewmodel: ResponderViewModel) -> Bool {
+    return viewmodel.apiPathVm.selectedEndpoint == .init(base: .availability, sub: .request, method: .post)
+}
+
 public struct ValuesPaneView: View {
     @EnvironmentObject var viewmodel: ResponderViewModel
 
     public init() {}
 
     public var body: some View {
-        // ScrollView {
-            VStack {
-                if viewmodel.apiPathVm.selectedRoute == .custom {
-                    CustomMessagePaneView()
-                    // .environmentObject(viewmodel)
-                } else if viewmodel.apiPathVm.selectedRoute == .appointment && !(viewmodel.apiPathVm.selectedEndpoint == .availabilityRequest) {
-                    // DatePickerView()
-                    DateAppointmentPickerView()
-                    // .environmentObject(viewmodel)
-                } else if viewmodel.apiPathVm.selectedRoute == .appointment && viewmodel.apiPathVm.selectedEndpoint == .availabilityRequest {
+        VStack {
+            if viewmodel.apiPathVm.selectedRoute == .custom {
+                CustomMessagePaneView()
+            // } else if viewmodel.apiPathVm.selectedRoute == .appointment && !(viewmodel.apiPathVm.selectedEndpoint == .availabilityRequest) {
+            } else if viewmodel.apiPathVm.selectedRoute == .appointment {
+                if isAvailReq(viewmodel) {
                     AppointmentAvailabilityPaneView()
                     .frame(maxWidth: 350)
-                } else  if (viewmodel.apiPathVm.selectedRoute == .quote && viewmodel.apiPathVm.selectedEndpoint?.base == .agreement) {
-                    QuoteAgreementPaneView()
-                    .frame(maxWidth: 350)
                 } else {
-                    if viewmodel.apiPathVm.endpointNeedsAvailabilityVariable {
-                        VStack(alignment: .leading, spacing: 8) {
-                            WeeklyScheduleView(viewModel: viewmodel.weeklyScheduleVm)
-                        }
-                        .padding(.vertical, 8)
+                    DateAppointmentPickerView()
+                }
+            // } else if viewmodel.apiPathVm.selectedRoute == .appointment && viewmodel.apiPathVm.selectedEndpoint == .init(base: .availability, sub: .request, method: .post) {
+            //     AppointmentAvailabilityPaneView()
+            //     .frame(maxWidth: 350)
+            } else  if (viewmodel.apiPathVm.selectedRoute == .quote && viewmodel.apiPathVm.selectedEndpoint?.base == .agreement) {
+                QuoteAgreementPaneView()
+                .frame(maxWidth: 350)
+            } else {
+                if viewmodel.apiPathVm.endpointNeedsAvailabilityVariable {
+                    VStack(alignment: .leading, spacing: 8) {
+                        WeeklyScheduleView(viewModel: viewmodel.weeklyScheduleVm)
                     }
+                    .padding(.vertical, 8)
                 }
             }
-        // }
+        }
     }
 }
